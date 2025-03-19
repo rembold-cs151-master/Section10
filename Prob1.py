f7a8827f9227c5cec61771a1b339000d622d48ac
Jed Rembold <jjrembold@willamette.edu>
2025-03-19 15:50:15 -0700

    Add actual PDF became Emma demanded and templates

---
+++ Prob1.py
@@ -0,0 +1,7 @@
+from pgl import GWindow, GCompound, GRect
+from random import uniform
+
+WIDTH = 600
+HEIGHT = 600
+
+
---
+++ Prob2.py
@@ -0,0 +1,15 @@
+from pgl import GWindow, GRect
+
+SEG_WIDTH = 20
+GW_WIDTH = 41 * SEG_WIDTH
+GW_HEIGHT = 31 * SEG_WIDTH
+DELTA_TIME = 150
+
+def snake():
+    """Runs a basic implementation of the game Snake"""
+
+    gw = GWindow(GW_WIDTH, GW_HEIGHT)
+
+
+if __name__ == "__main__":
+    snake()
