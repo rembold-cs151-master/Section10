8f9e1c98866e5fabe819a1c3549fdab7a39430c2
Jed Rembold <jedediah.r@gmail.com>
2025-11-05 00:15:48 -0800

    Update to Fall, with all Sample Exam problems

---
+++ Prob2a3.py
@@ -0,0 +1,21 @@
+
+puzzle = [ ["R", "B", "R"],
+           ["R", "B", "B"],
+           ["B", "R", "R"] ]
+
+bad_puzzle = [ ["R", "B", "R", "B"],
+               ["R", "B", "B", "R"],
+               ["R", "R", "R", "B"],
+               ["R", "B", "B", "R"] ]
+
+
+def no_duplicate_rows(puzzle):
+    """Predicate function to determine if there are any duplicate rows in the puzzle"""
+
+
+def no_3petes_in_rows(puzzle):
+    """Predicate function to determine if any rows contain 3 or more adjacent
+    identical characters.
+    """
+
+
---
+++ Prob2b3.py
@@ -0,0 +1,7 @@
+
+digits = [0,1,2,3,4,5,6,7,8,9]
+
+def rotate_array(a_list, shift_amt):
+    """Rotates elements in a list forwards or backwards, according to the shift amount.
+    Does not create a new list, but mutates the list that is passed in.
+    """
